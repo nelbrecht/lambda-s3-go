fa7abfc84b1f7ba95a5a41561a77d40d8b28b82b
Norbert Elbrecht <elbrecht@dimensional.de>
2022-04-27 15:36:03 +0200

    Log S3Event payload and environment

--- lambda/handler.go
+++ lambda/handler.go
@@ -2,13 +2,32 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
+	"log"
+	"os"
 
+	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 )
 
-func HandleRequest(ctx context.Context, name interface{}) (string, error) {
-	return fmt.Sprintf("Hello %#v!", name), nil
+func HandleRequest(ctx context.Context, event events.S3Event) (string, error) {
+	log.Printf("Typed EVENT: %#v", event)
+
+	eventJson, _ := json.Marshal(event)
+	log.Printf("Unformatted EVENT: %s", eventJson)
+
+	eventJson, _ = json.MarshalIndent(event, "", "  ")
+	log.Printf("EVENT: %s", eventJson)
+
+	// environment variables
+	log.Printf("REGION: %s", os.Getenv("AWS_REGION"))
+	log.Println("ALL ENV VARS:")
+	for _, element := range os.Environ() {
+		log.Println(element)
+	}
+
+	return fmt.Sprintf("Hello %#v!", event), nil
 }
 
 func main() {
